0d49c0377fea12704afe964f6a22eeb3da71ddfc
John Hunter <johnwrongway@gmail.com>
2018-12-16 21:09:29 -0800

    settings made

--- Part2/alien/alien_invasion.py
+++ Part2/alien/alien_invasion.py
@@ -1,22 +1,32 @@
 import sys
 
 import pygame
+from settings import Settings
 
 def run_game():
-    # Init game and create a screen objectself.
+    # Init game and create a screen object settings.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    #background color
+    #bg_color = (230, 230, 230)
+
 
     #Start the main loop for the game
     while True:
 
         #Watch for keyboard and mouse events.
-        for event.type == pygame.QUIT
-            sys.exit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
+        #Redraw bg everyloop
+        screen.fill(ai_settings.bg_color)
 
         #make the most recently drawn screen visible
         pygame.display.flip()
+
 
 run_game()
---
+++ Part2/alien/settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    #A clas to store all settings for alien Invasion
+
+    def __init__(self):
+        #INIT the game's settings
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (20, 230, 230)
